7979c84e15b3388e018c9d07358a49dc71d08000
gitkado <gitkado@gmail.com>
2021-05-13 00:43:47 +0900

    feat: user authentication

--- now-questionnaire/src/App.tsx
+++ now-questionnaire/src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Amplify from 'aws-amplify'
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components'
+import awsconfig from './aws-exports'
+import { AmplifyAuthenticator, AmplifySignOut, AmplifySignUp } from "@aws-amplify/ui-react";
+
+Amplify.configure(awsconfig);
 
 function App() {
-  return (
+  const [authState, setAuthState] = React.useState<AuthState | null>()
+  const [user, setUserState] = React.useState<any>()
+
+  React.useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState)
+      setUserState(authData)
+    })
+  }, [])
+
+  return authState === AuthState.SignedIn && user ? (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Hello, {user.username}</div>
+      <AmplifySignOut />
     </div>
-  );
+  ) : (
+    <AmplifyAuthenticator>
+      <AmplifySignUp
+        slot="sign-up"
+        formFields={[
+          { type: "username" },
+          { type: "password" },
+          { type: "email" },
+        ]}
+      />
+    </AmplifyAuthenticator>
+  )
 }
 
-export default App;
+export default App
